f94ac2d360a5080bd17c255eba231fc5155fc389
Nikita Stetskiy <nikin929@gmail.com>
2020-10-12 17:28:37 +0200

    Se ha mejorado un poco la clase events :pencil2:, closes #8

--- src/eventscalendar/events.js
+++ src/eventscalendar/events.js
@@ -1,13 +1,23 @@
 class events{
-    constructor(fecha){
+    constructor(fecha, evento){
+        //this.fecha = Object.assign(new Date(), fecha);
         this.fecha = fecha;
+        this.evento = evento;
     }
 
     getEvent(){
+        //onsole.log(this.fecha.toString());
+        //console.log(this.evento.toString());
         throw new Error('Sin implementar');
     }
 
-    setEvent(){
+    setEvent(fecha, evento){
+        //this.fecha = fecha;
+        //this.evento = evento;
         throw new Error('Sin implementar');
     }
 }
+
+// Se ha utilizado para probar cosas del código ( se borrará en un futuro)
+//var v = new events(new Date(2021, 7, 2, 0, 0, 0, 0), "Nuevo evento para un cumple");
+//v.getEvent();
