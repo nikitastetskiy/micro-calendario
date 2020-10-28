5a2ba31c10cde9f2bda5efd592498029547dfa22
Nikita Stetskiy <nikin929@gmail.com>
2020-10-28 13:38:19 +0100

    Arreglo y mejoro los test, HU: #4 #5 #6

--- test/util.test.js
+++ test/util.test.js
@@ -8,27 +8,32 @@ var events = require('../src/eventscalendar/events');
 test('Test de la función translate', () => {
     var plan = new planner();
     var secuencia = plan.translate('2020-10-17T17:44:00 Evento 1');
-    var final = secuencia.toString();
-    expect(final).toBe('Sat Oct 17 2020 17:44:00 GMT+0200 (GMT+02:00) Evento 1');
+    var secuencia_esperada = new events(new Date('2020-10-17T17:44:00'), "Evento 1");
+    expect(secuencia.toString()).toEqual(secuencia_esperada.toString());
+    expect(secuencia).toMatchObject(secuencia_esperada);
 
     secuencia = plan.translate('2020-10-17T17:44:0Evento 1');
     final = secuencia.toString();
     expect(final).toBe("false");
 
     secuencia = plan.translate('2020-10-17T17:44:00');
-    final = secuencia.toString();
-    expect(final).toBe("Sat Oct 17 2020 17:44:00 GMT+0200 (GMT+02:00)  ");
+    secuencia_esperada = new events(new Date('2020-10-17T17:44:00'), " ");
+    expect(secuencia.toString()).toEqual(secuencia_esperada.toString());
+    expect(secuencia).toMatchObject(secuencia_esperada);
 });
 
 //planner - getEvent y addEvent
 test('Test de la función getEvent y addEvent', () => {
     var plan = new planner();
     var secuencia = plan.translate('2020-10-17T17:44:00 Evento 1');
+    var secuencia_esperada = new events(new Date('2020-10-17T17:44:00'), "Evento 1");
     plan.addEvent(secuencia);
 
     var secuencia2 = plan.getEvent(0);
-    var final = secuencia2.toString();
-    expect(final).toBe('Sat Oct 17 2020 17:44:00 GMT+0200 (GMT+02:00) Evento 1');
+    expect(secuencia.toString()).toEqual(secuencia2.toString());
+    expect(secuencia.toString()).toEqual(secuencia_esperada.toString());
+    expect(secuencia).toMatchObject(secuencia_esperada);
+    expect(secuencia).toMatchObject(secuencia2);
 
     var secuencia3 = plan.translate('2020-1717:44:00');
     expect(() => {plan.addEvent(secuencia3)}).toThrow(Error('Mal declarado el evento'));
@@ -39,9 +44,10 @@ test('Test de la función getEvent y addEvent', () => {
 test('Test de la función Convertir UTC', () => {
     var plan = new planner();
     var secuencia = plan.convertirUTC(new Date ('2020-10-17T17:44:00'));
-    var final = secuencia.toISOString();
+    var secuencia_esperada = (new Date ('2020-10-17T17:44:00'));
 
-    expect(final).toBe('2020-10-17T15:44:00.000Z');
+    expect(secuencia.toISOString()).toEqual(secuencia_esperada.toISOString());
+    expect(secuencia).toMatchObject(secuencia_esperada);
 });
 
 // planner - validar secuencia
@@ -60,12 +66,16 @@ test('Test de la función Validar Secuencia', () => {
 //events - getFecha, getMotivo, setEvent y toString
 test('Test de la función getFecha, getMotivo, setEvent y toString', () => {
     var v = new events(new Date(2021, 7, 2, 0, 0, 0, 0), "Nuevo evento para un cumple");
-    expect(v.getFecha().toISOString()).toBe('2021-08-01T22:00:00.000Z');
-
+    var date = new Date(2021, 7, 2, 0, 0, 0, 0);
+    expect(v.getFecha().toString()).toEqual(date.toString());
+    expect(v.getFecha()).toMatchObject(date);
     expect(v.getMotivo()).toBe('Nuevo evento para un cumple');
 
     v.setEvent(new Date(2020, 8, 2, 13, 27, 45, 0), "Otro cumple");
-    expect(v.toString()).toBe("Wed Sep 02 2020 13:27:45 GMT+0200 (GMT+02:00) Otro cumple");
+    date = new Date(2020, 8, 2, 13, 27, 45, 0);
+    expect(v.getFecha().toString()).toEqual(date.toString());
+    expect(v.getFecha()).toMatchObject(date);
+    expect(v.getMotivo()).toBe('Otro cumple');
 
     expect(() => {v.setEvent(null, "Otro cumple")}).toThrow(Error('Evento mal puesto'));
     expect(() => {v.setEvent(new Date(), null)}).toThrow(Error('Evento mal puesto'));
