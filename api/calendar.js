ba9621afd781da897973e3634c192462d12336a8
Nikita Stetskiy <nikin929@gmail.com>
2020-11-25 22:43:07 +0100

    Incluyo Vercel y añado función Calendar #19

--- api/calendar.js
+++ api/calendar.js
@@ -1,4 +1,29 @@
+const Planner = require('../src/eventscalendar/planner');
+
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    res.status(200).send(`Hello ${name}!`);
+    // La fecha o el evento será introducido en la url
+    // fecha por defecto = ''
+    const { fecha = '' } = req.query;
+    // El resultado que enviaremos finalmente
+    let mensaje;
+    if (fecha === '') {
+        mensaje = 'Evento mal introducido (vacio).';
+    }
+    // El objeto tipo planner que usaremos para traducir
+    // la fecha introducida por el usuario
+    // Se convierte en tipo evento y hacemos toString
+    const planner = new Planner();
+    const evento = planner.translate(`${fecha}`);
+    if (evento === null || evento === false) {
+        mensaje = `Evento mal introducido.`;
+    } else {
+        // mensaje = evento.toString();
+        const objetoJSON = {
+            Fecha: `${evento.fecha.toString()}`,
+            Motivo: `${evento.motivo.toString()}`,
+        };
+        mensaje = JSON.stringify(objetoJSON);
+    }
+
+    res.status(200).send(mensaje);
 };
