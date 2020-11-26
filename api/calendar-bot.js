63c2d4bd630db821bc3047d00187de97de123689
Nikita Stetskiy <nikin929@gmail.com>
2020-11-26 16:45:22 +0100

    Incluyo Telegram bot y bug de Vercel #21

--- api/calendar-bot.js
+++ api/calendar-bot.js
@@ -1,43 +1,38 @@
 const Planner = require('../src/eventscalendar/planner');
 
 module.exports = async (req, res) => {
-    let mensaje = '';
     // Usamos try y comprobamos el body por si hay algún error
-    try {
-        if (req.body.message !== undefined) {
-            // Pillamos el body
-            const { text } = req.body.message;
-            // Comprobamos el contenido del mensaje
-            if (text && text.charAt(0) === '/') {
-                if (text === '/help') {
-                    mensaje = 'Hola !';
+    if (req.body.message !== undefined) {
+        let mensaje = '';
+        // Pillamos el body
+        const { text } = req.body.message;
+        // Comprobamos el contenido del mensaje
+        if (text && text.charAt(0) === '/') {
+            if (text === '/help') {
+                mensaje = 'Hola !';
+            } else {
+                const planner = new Planner();
+                mensaje = `${text.slice(1, text.length)}`;
+                const evento = planner.translate(`${mensaje}`);
+                if (evento === null || evento === false) {
+                    mensaje = `Evento mal introducido.`;
                 } else {
-                    const planner = new Planner();
-                    mensaje = `${text.slice(1, text.length)}`;
-                    const evento = planner.translate(`${mensaje}`);
-                    if (evento === null || evento === false) {
-                        mensaje = `Evento mal introducido.`;
-                    } else {
-                        mensaje = evento.toString();
-                    }
+                    mensaje = evento.toString();
                 }
-                // Creamos JSON con el mensaje generado
-                // con el id del chat, el metodo de telegram
-                // y response tipo json y utf-8 (nos lo pide
-                // telegram)
-                const objetoJSON = {
-                    text: mensaje,
-                    method: 'sendMessage',
-                    chat_id: req.body.message.chat.id,
-                };
-                res.setHeader(
-                    'Content-Type',
-                    'application/json; charset=utf-8'
-                );
-                res.status(200).json(objetoJSON);
             }
-        } else {
-            res.status(200).send('Bot Micro - Calendario');
+            // Creamos JSON con el mensaje generado
+            // con el id del chat, el metodo de telegram
+            // y response tipo json y utf-8 (nos lo pide
+            // telegram)
+            const objetoJSON = {
+                text: mensaje,
+                method: 'sendMessage',
+                chat_id: req.body.message.chat.id,
+            };
+            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+            res.status(200).json(objetoJSON);
         }
-    } catch (error) {}
+    } else {
+        res.status(200).send('Bot Micro - Calendario');
+    }
 };
