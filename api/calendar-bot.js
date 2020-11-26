c018a571a88d4cccc060b465c4cb6cf00430d3df
Nikita Stetskiy <nikin929@gmail.com>
2020-11-26 19:51:36 +0100

    Incluyo Telegram bot y subo su función de nuevo [skip travis] #21

--- api/calendar-bot.js
+++ api/calendar-bot.js
@@ -2,38 +2,42 @@ const Planner = require('../src/eventscalendar/planner');
 
 module.exports = async (req, res) => {
     // Usamos try y comprobamos el body por si hay algún error
-    if (req.body !== undefined || req.body.message !== undefined) {
-        let mensaje = '';
-        // Pillamos el body
-        const { text } = req.body.message;
-        // Comprobamos el contenido del mensaje
-        if (text === '/help' || text.charAt(0) !== '/') {
-            mensaje =
-                'Para crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1" ó "/1995-12-17T03:24:00".';
-        } else if (text === '/start') {
-            mensaje =
-                'Hola !\nBot Micro-Calendario a su servicio!\nPara crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1".';
-        } else {
-            const planner = new Planner();
-            mensaje = `${text.slice(1, text.length)}`;
-            const evento = planner.translate(`${mensaje}`);
-            if (evento === null || evento === false) {
-                mensaje = `Evento mal introducido.`;
+    if (req.body !== undefined) {
+        if (req.body.message !== undefined) {
+            let mensaje = '';
+            // Pillamos el body
+            const { text } = req.body.message;
+            // Comprobamos el contenido del mensaje
+            if (text === '/help' || text.charAt(0) !== '/') {
+                mensaje =
+                    'Para crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1" ó "/1995-12-17T03:24:00".';
+            } else if (text === '/start') {
+                mensaje =
+                    'Hola !\nBot Micro-Calendario a su servicio!\nPara crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1".';
             } else {
-                mensaje = `Se ha creado evento en ${evento.toString()}`;
+                const planner = new Planner();
+                mensaje = `${text.slice(1, text.length)}`;
+                const evento = planner.translate(`${mensaje}`);
+                if (evento === null || evento === false) {
+                    mensaje = `Evento mal introducido.`;
+                } else {
+                    mensaje = `Se ha creado evento en ${evento.toString()}`;
+                }
             }
+            // Creamos JSON con el mensaje generado
+            // con el id del chat, el metodo de telegram
+            // y response tipo json y utf-8 (nos lo pide
+            // telegram)
+            const objetoJSON = {
+                text: mensaje,
+                method: 'sendMessage',
+                chat_id: req.body.message.chat.id,
+            };
+            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+            res.status(200).json(objetoJSON);
+        } else {
+            res.status(200).send('Hola !\nBot Micro-Calendario a su servicio!');
         }
-        // Creamos JSON con el mensaje generado
-        // con el id del chat, el metodo de telegram
-        // y response tipo json y utf-8 (nos lo pide
-        // telegram)
-        const objetoJSON = {
-            text: mensaje,
-            method: 'sendMessage',
-            chat_id: req.body.message.chat.id,
-        };
-        res.setHeader('Content-Type', 'application/json; charset=utf-8');
-        res.status(200).json(objetoJSON);
     } else {
         res.status(200).send('Hola !\nBot Micro-Calendario a su servicio.');
     }
