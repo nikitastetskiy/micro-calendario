d920c530d7dca3ed5c2e4ed458e794ebbe083a29
Nikita Stetskiy <nikin929@gmail.com>
2020-12-03 19:35:57 +0100

    Mejoro la función Netlify GraphQL para una mejor integración en el proyecto #20

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -6,11 +6,18 @@ const typeDefs = gql`
         allAuthors: [Author!]
         author(id: Int!): Author
         authorByName(name: String!): Author
+        allEvents: [Evento!]
+        eventsByFecha(name: String!): Evento
     }
     type Author {
         id: ID!
         name: String!
         married: Boolean!
+    }
+    type Evento {
+        id: ID!
+        fecha: String!
+        motivo: String!
     }
 `;
 
@@ -18,6 +25,12 @@ const authors = [
     { id: 1, name: 'Terry Pratchett', married: false },
     { id: 2, name: 'Stephen King', married: true },
     { id: 3, name: 'JK Rowling', married: false },
+];
+
+const events = [
+    { id: 1, fecha: '13 de Septiembre', motivo: 'Comprar regalos de Navidad' },
+    { id: 2, fecha: '22 de Octubre', motivo: 'Cumpleaños Gabriel' },
+    { id: 3, fecha: '1 de Enero de 2021', motivo: 'Feliz año Nuevo!' },
 ];
 
 const resolvers = {
@@ -34,6 +47,15 @@ const resolvers = {
             return (
                 authors.find((author) => author.name === args.name) ||
                 'NOTFOUND'
+            );
+        },
+        allEvents: () => {
+            return events;
+        },
+        eventsByFecha: (root, args) => {
+            console.log('hihhihi', args.fecha);
+            return (
+                events.find((event) => event.fecha === args.fecha) || 'NOTFOUND'
             );
         },
     },
