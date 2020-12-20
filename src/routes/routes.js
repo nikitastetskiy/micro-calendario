0266d30e9517b7d8720bed53be0c531bd364d4af
Nikita Stetskiy <nikin929@gmail.com>
2020-12-20 16:57:39 +0100

    Añado pureba con Express.js #6, para probar el tiempo de ejecución

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,46 @@
-"use strict";
+/* eslint strict: ["error", "global"] */
+
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const Planner = require('../eventscalendar/planner');
+
+const planner = new Planner();
+
+// Crea un evento
+app.put('/eventscalendar/', function (req, res) {
+    // La fecha o el evento será introducido en la URL
+    // fecha por defecto = ''
+    // Entrada URL query parameters (después del ? en la URL)
+    const { fecha = '' } = req.query;
+    // El resultado que enviaremos finalmente
+    let mensaje;
+    if (fecha === '') {
+        mensaje = 'Evento mal introducido (vacio).';
+    }
+    // El objeto tipo planner que usaremos para traducir
+    // la fecha introducida por el usuario
+    // Se convierte en tipo evento y hacemos toString
+    const evento = planner.translate(`${fecha}`);
+    if (evento === null || evento === false) {
+        mensaje = `Evento mal introducido.`;
+    } else {
+        planner.addEvent(evento);
+        const objetoJSON = {
+            Fecha: `${evento.fecha.toString()}`,
+            Motivo: `${evento.motivo.toString()}`,
+        };
+        mensaje = objetoJSON;
+    }
+    // Establecemos código de estado estándar (200)
+    // Con res enviamos la función send, esta
+    // contiene un string
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(mensaje);
+});
+
+// Exportamos la variable para los tests
+module.exports = app;
