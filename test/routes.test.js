5f0552c52ca1db28a39508c119b04723a6ec4b1e
Nikita Stetskiy <nikin929@gmail.com>
2020-12-20 17:59:08 +0100

    Añado test con Hapi #17, para probar el tiempo de ejecución

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,12 +1,33 @@
 // eslint-disable-next-line node/no-unpublished-require
 const request = require('supertest');
-const app = require('../src/routes/routes.js');
+const server = require('../src/routes/routes.js');
 
-describe('Test de eventscalendar/?', () => {
-    test('Creación de evento - PUT', () =>
-        request(app)
-            .put('/eventscalendar/')
-            .then((response) => {
-                expect(response.statusCode).toBe(200);
-            }));
+// Start application before running the test case
+beforeAll(
+    () =>
+        new Promise((done) => {
+            server.events.on('start', () => {
+                done();
+            });
+        })
+);
+
+// Stop application after running the test case
+afterAll(
+    () =>
+        new Promise((done) => {
+            server.events.on('stop', () => {
+                done();
+            });
+            server.stop();
+        })
+);
+
+test('should success with server connection', async () => {
+    const options = {
+        method: 'PUT',
+        url: '/eventscalendar',
+    };
+    const data = await server.inject(options);
+    expect(data.statusCode).toBe(200);
 });
