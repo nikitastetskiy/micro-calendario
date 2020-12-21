6f3167cc488d77b44047175d163f590269b82963
Nikita Stetskiy <nikin929@gmail.com>
2020-12-21 17:32:44 +0100

    Añado Express como Framework y su test para Consultar Evento #17

--- test/routes.test.js
+++ test/routes.test.js
@@ -27,4 +27,25 @@ describe('Creación de evento y calendario', () => {
                     done();
                 });
         }));
+    it('should return correct type and text of all events - GET', () =>
+        new Promise((done) => {
+            request(app)
+                .get('/eventscalendar/')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+            request(app)
+                .get('/eventscalendar/')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((error, mensaje) => {
+                    console.log(mensaje.text);
+                    if (error) {
+                        return done(error);
+                    }
+                    mensaje.text.should.equal(
+                        '{"Fecha":"Sun Dec 17 1995 03:24:00 GMT+0100 (GMT+01:00)","Motivo":"Evento 1"}'
+                    );
+                    done();
+                });
+        }));
 });
