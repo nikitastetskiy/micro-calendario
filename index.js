27c1fcd2b77baabb48ea6bafcaad2cb062d31701
Nikita Stetskiy <nikin929@gmail.com>
2021-01-09 17:24:09 +0100

    Añado estructura básica index.js #26

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN || '';
 
 app.get('/', (req, res) => res.send('Hello World - Micro-Calendario!'));
 
 app.use(bodyParser.json());
 
-app.post('/webhooks/telegram', async (req, res) => {
+app.post('/webhooks/telegram', (req, res) => {
     console.log(req.body);
 
     res.send({status: 'ok'});
