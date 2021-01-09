7ddc515ae1b224828189d5526b79a335335d7776
Nikita Stetskiy <nikin929@gmail.com>
2021-01-09 19:16:33 +0100

    Dubugging ...  #26

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.send('Hello World - Micro-Calendario!'));
 app.post('/webhooks/telegram', (req, res) => {
     console.log(req.body);
     const user = User.createFromRequest(req.body)[0];
-    console.log(user.conversationId);
+    // console.log(user.conversationId);
     const objetoJSON = {
         text: 'Hola',
         method: 'sendMessage',
