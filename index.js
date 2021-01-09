344068b50f00f335d9a3930bac49b0cab5637b3c
Nikita Stetskiy <nikin929@gmail.com>
2021-01-09 17:00:23 +0100

    Añado token en index #26

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN || '';
 
 app.get('/', (req, res) => res.send('Hello World - Micro-Calendario!'));
 
