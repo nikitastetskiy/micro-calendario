b266900573a7a8db5a4e33808cb1945399bd5f29
Nikita Stetskiy <nikin929@gmail.com>
2021-01-09 19:07:11 +0100

    Mejoro index y añado usuario #26

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const User = require('/models/user');
+const User = require('./models/user');
+const sendMessage = require('./src/telegram/telegram');
+
+[User].map((model) => model.sync({ force: false }));
 
 app.use(bodyParser.json());
 
@@ -11,8 +14,16 @@ app.get('/', (req, res) => res.send('Hello World - Micro-Calendario!'));
 
 app.post('/webhooks/telegram', (req, res) => {
     console.log(req.body);
-
-    res.send({ status: 'ok' });
+    const user = User.createFromRequest(req.body)[0];
+    console.log(user.conversationId);
+    const objetoJSON = {
+        text: 'Hola',
+        method: 'sendMessage',
+        chat_id: req.body.message.chat.id,
+    };
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.status(200).json(objetoJSON);
+    // res.send({ status: 'ok' });
 });
 
 app.listen(port, () =>
