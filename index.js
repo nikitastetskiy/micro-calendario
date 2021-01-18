8b2d0b427aabbe1d7e030e6da953a4f71c9f622a
Nikita Stetskiy <nikin929@gmail.com>
2021-01-18 15:06:53 +0100

    Pruebo cambiando las cabeceras #26

--- index.js
+++ index.js
@@ -20,56 +20,48 @@ app.get('/', (req, res) => res.send('Hello World - Micro-Calendario!'));
 app.get('/status', (req, res) => res.send({ status: 'OK' }));
 
 app.post('/webhooks/telegram', async (req, res) => {
-    if (req.body !== undefined) {
-        if (req.body.message !== undefined) {
-            let mensaje = '';
-            // Pillamos el body
-            const { text } = req.body.message;
-            // Comprobamos el contenido del mensaje
-            if (text === '/help' || text.charAt(0) !== '/') {
-                mensaje =
-                    'Para crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1" ó "/1995-12-17T03:24:00".';
-            } else if (text === '/start') {
-                mensaje =
-                    'Hola !\nBot Micro-Calendario a su servicio!\nPara crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1".';
-            } else {
-                const planner = new Planner();
-                mensaje = `${text.slice(1, text.length)}`;
-                const evento = planner.translate(`${mensaje}`);
-                if (evento === null || evento === false) {
-                    mensaje = `Evento mal introducido. Use "/help" para la lista de comandos.`;
-                } else {
-                    const id = req.body.message.from.id.toString();
-                    const chat = req.body.message.chat.id;
-                    const fec = evento.getFecha();
-                    const mot = evento.getMotivo().toString();
-                    const eve = await userDB.createEvento(id, chat, fec, mot);
-                    res.location(
-                        `/webhooks/telegram/${
-                            JSON.parse(JSON.stringify(eve))._id
-                        }`
-                    );
-                    mensaje = `Se ha creado evento en ${evento.toString()}`;
-                }
-            }
-            // Creamos JSON con el mensaje generado
-            // con el id del chat, el metodo de telegram
-            // y response tipo json y utf-8 (nos lo pide
-            // telegram)
-            const objetoJSON = {
-                text: mensaje,
-                method: 'sendMessage',
-                chat_id: req.body.message.chat.id,
-            };
-            res.setHeader('Content-Type', 'application/json; charset=utf-8');
-            res.status(201).json(objetoJSON);
+    try {
+        let mensaje = '';
+        // Pillamos el body
+        const { text } = req.body.message;
+        // Comprobamos el contenido del mensaje
+        if (text === '/help' || text.charAt(0) !== '/') {
+            mensaje =
+                'Para crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1" ó "/1995-12-17T03:24:00".';
+        } else if (text === '/start') {
+            mensaje =
+                'Hola !\nBot Micro-Calendario a su servicio!\nPara crear eventos use este formato: "/1995-12-17T03:24:00 Evento 1".';
         } else {
-            console.log(req);
-            res.status(400).send('FIELD_MESSAGE_EMPTY');
+            const planner = new Planner();
+            mensaje = `${text.slice(1, text.length)}`;
+            const evento = planner.translate(`${mensaje}`);
+            if (evento === null || evento === false) {
+                mensaje = `Evento mal introducido. Use "/help" para la lista de comandos.`;
+            } else {
+                const id = req.body.message.from.id.toString();
+                const chat = req.body.message.chat.id;
+                const fec = evento.getFecha();
+                const mot = evento.getMotivo().toString();
+                const eve = await userDB.createEvento(id, chat, fec, mot);
+                res.location(
+                    `/webhooks/telegram/${JSON.parse(JSON.stringify(eve))._id}`
+                );
+                mensaje = `Se ha creado evento en ${evento.toString()}`;
+            }
         }
-    } else {
-        console.log(req);
-        res.status(400).send('FIELD_BODY_EMPTY');
+        // Creamos JSON con el mensaje generado
+        // con el id del chat, el metodo de telegram
+        // y response tipo json y utf-8 (nos lo pide
+        // telegram)
+        const objetoJSON = {
+            text: mensaje,
+            method: 'sendMessage',
+            chat_id: req.body.message.chat.id,
+        };
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.status(201).json(objetoJSON);
+    } catch (e) {
+        res.status(400).json(e);
     }
 });
 
